1a190bf6c98c832df24bf7ac0328141ee59aaff6
MatheusCBarreto <devmattbarreto@protonmail.com>
2023-07-09 15:21:26 -0300

    criando tabelas de categorias, sincronizando com o banco de dados  e adicionado função de delete

--- categories/categoriesController.js
+++ categories/categoriesController.js
@@ -22,4 +22,29 @@ router.post('/categories/save', (req, res) => {
   }
 });
 
+router.get('/admin/categories', (req, res) => {
+  Category.findAll().then((categories) => {
+    res.render('admin/categories/index', { categories: categories });
+  });
+});
+
+router.post('/categories/delete', (req, res) => {
+  let id = req.body.id;
+  if (id != undefined) {
+    if (!isNaN(id)) {
+      Category.destroy({
+        where: {
+          id: id,
+        },
+      }).then(() => {
+        res.redirect('/admin/categories');
+      });
+    } else {
+      res.redirect('/admin/categories');
+    }
+  } else {
+    res.redirect('/admin/categories');
+  }
+});
+
 module.exports = router;
